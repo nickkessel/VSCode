1299a3ce0b6392efe93bb154aa4d58ad7592a5e9
Nick Kessel <nickkessel22@protonmail.com>
2023-09-19 19:17:38 -0400

    threading

--- threading/main.py
+++ threading/main.py
@@ -6,6 +6,25 @@ def count(countTo):
     for x in range(countTo):
         print(x)
         time.sleep(.1)
+        
+ob = t.Turtle()
+ob.pu()
+ob.goto(-100,100)
+ob.speed(0)
 
-t1 = threading.Thread(target=count, args=(100,))
+def drawShape(sides):
+    for x in range(sides):
+        ob.pd()
+        ob.fd(50)
+        ob.right(360/sides)
+        ob.fd(50)
+
+t1 = threading.Thread(target=count, args=(20,))
+t2 = threading.Thread(target=drawShape, args=(40,))
 t1.start()
+t2.start()
+
+#t1.join()
+
+wn = t.Screen()
+wn.mainloop()
