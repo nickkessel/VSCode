2958f19b2e65da57b3601d6eececfacddd1f7362
Nick Kessel <nickkessel22@protonmail.com>
2023-09-19 18:45:12 -0400

    working on it

--- randomlines.py
+++ randomlines.py
@@ -6,7 +6,17 @@ import random as r
 #----------variables
 font_setup = ["Impact", 35, "normal"]
 screen_size = 350
-
+colors = ['white',
+    'black',
+    'red',
+    'green',
+    'blue',
+    'cyan',
+    'magenta',  # You can also use 'purple'
+    'yellow',
+    'orange',
+    'brown']
+values = [20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70,110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 149, 150, 151, 152, 153, 154, 155, 156, 157, 158, 159, 160]
 
 #---------turtle setup
 
@@ -65,6 +75,7 @@ border.goto(-screen_size,screen_size + 20)
 #main generate function
 def design(x,y):
     start.color("red")
+    ob.color(r.choice(colors))
     for i in range(200):
         clear.color("blue")
         if abs(ob.xcor()) > screen_size or abs(ob.ycor()) > screen_size:
@@ -72,8 +83,9 @@ def design(x,y):
             ob.goto(0,0)
             ob.pd()
         else:  
-            ob.fd(r.randint(20,60))
-            ob.right(r.randint(45,135))
+            ob.fd(r.randint(25,55))
+            #ob.right(r.randint(40,140))
+            ob.right(r.choice(values))
     start.color("green")
         
 #clears screen       
