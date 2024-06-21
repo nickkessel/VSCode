c1985d2d9e7b41c297ad633702da2591bb817a9b
Nick Kessel <nickkessel22@protonmail.com>
2024-06-21 10:12:42 -0400

    more stuff

--- ambient-weather/main.py
+++ ambient-weather/main.py
@@ -13,9 +13,9 @@ http = urllib3.PoolManager()
 
 response_body = http.request('GET', 'https://rt.ambientweather.net/v1/devices?3abc1d7d9a604ba6acea3b40bd836ac7078d323cc748473a8edf89eaa1d75d30=&47920d81820947199710d843bc624257328d689b65c74bcc81bc2b94b0f391c0=')
 print(response_body)
-# api = AmbientAPI()
-# devices = api.get_devices()
-# device1 = devices[0]
+api = AmbientAPI()
+devices = api.get_devices()
+device1 = devices[0]
 
 # time.sleep(1)
-# print(device1.get_data())
+print(device1.get_data())
